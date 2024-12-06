d6f47009937b601b16db65e7b07c998d2634f83f
Regi Shehi <regi_shehi@hotmail.com>
2024-12-06 17:52:54 +0100

    Add Domain project

---
+++ src/StudentEnrollment.Domain/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentEnrollment.Data;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime UpdatedDate { get; set; }
+    public string? UpdatedBy { get; set; }
+}
